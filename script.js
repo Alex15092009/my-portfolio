f2d455bbb83b3de7dff41b25b21e0beb59a96ab9
Alex15092009 <xxxgggtttooo@gmail.com>
2026-04-03 08:31:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,15 @@ const swiper = new Swiper('.cases-slider', {
         }
     }
 });
+// Плавная прокрутка для всех ссылок-якорей
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
